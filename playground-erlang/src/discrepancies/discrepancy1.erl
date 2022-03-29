480b1fb895cfff978d95154622decc8490b34b61
Frederik Flint <frederikflint@gmail.com>
2022-03-29 15:31:52 +0200

    fehrn siger gem

--- playground-erlang/src/discrepancies/discrepancy1.erl
+++ playground-erlang/src/discrepancies/discrepancy1.erl
@@ -1,18 +1,18 @@
 -module(discrepancy1).
--export([handle_stuff/2, handle_call/3]).
+-export([handle_stuff/2, handle_call/3, my_api/2]).
 
 -record(state, {num :: atom(), maskine :: number()}).
 -record(batman, {num :: atom()}).
 
-handle_stuff(Pid, #state.num = _Var) when is_atom(_Var) ->
+handle_stuff(Pid, _Arg) when is_atom(_Arg) ->
   my_api(Pid, {my_api_server, abe, abe11});
-handle_stuff(Pid, #batman.num = _Var) ->
-  my_api(Pid, {my_api_server, abe, abe}).
+handle_stuff(Pid, _Arg2) ->
+  my_api(Pid, {my_api_server, abe}).
 
 my_api(Pid, Command) ->
   gen_server:call(Pid, Command).
 
-handle_call({my_api_server, _Arg, #state.num}, _From, _State) ->
+handle_call({my_api_server, _Arg}, _From, _State) when is_atom(_Arg) ->
   {reply, atomman, _State};
-handle_call({my_api_server, _Arg, #batman.num}, _From, _State) ->
+handle_call({my_api_server, _Arg}, _From, _State) ->
   {reply, bob, _State}.
